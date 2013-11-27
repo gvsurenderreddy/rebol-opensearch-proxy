1407f14f3c6469913c33a643fa082c9bb9dc2322
U-MONIGLE\gparks <gparks@DM_Grant.monigle.com>
2013-11-27 16:33:37 -0700

    nice new version that handles looping on the load and some other things for you

--- opensearch.r
+++ opensearch.r
@@ -1,13 +1,16 @@
-REBOL [ 
-	Title: "Returns Opensearch results formatted for MS Windows search" 
+REBOL [
+	Title: "Returns Opensearch results formatted for MS Windows search"
 	Purpose: {
 		Provide a generic base for custom searches to be integrated into Windows
 
 		windows-search-connector will be the listener that Windows Explorer sends
-		requests to.  It provides basic sanitizing of query then calls extended 
-		search function, which returns formatted results.
+		requests to.  It provides basic sanitizing of query then calls extended
+		search function, which returns formatted results (???).
 
-		Your extended object!'s load and search functions may call a service, query a db or scrape a web startPage
+		Your extended object!'s search function may call a service, query a db or scrape a web startPage
+		(It can also optionally cause change.)
+
+		The extended object! can supply an optional load-search-data function
 	}
 
 	File: %opensearch.r
@@ -18,7 +21,7 @@ REBOL [
 	Version: 0.0.1
 	History: {
 		BOO!
-		0.0.1	Copied from my existing specific searches 
+		0.0.1	Copied from my existing specific searches
 				GOAL: create a generic base object! that will do everything
 				necessary to provide a user designed search from Windows Explorer
 	}
@@ -32,7 +35,7 @@ REBOL [
 ; service using functions and propeties in the object that's passed.
 ;
 opensearch: use [
-	minimum-length sanitize-query get-request-from http-port 
+	minimum-length sanitize-query get-request-from http-port
 	buffer-out cleanup next-time
 ][
 	unless connected? [to-error "No internet connection found. Please check your connection."]
@@ -47,12 +50,16 @@ opensearch: use [
 		unset [http-port server-port searchTerm results buffer-out]
 	]
 
-	; Makes a COPY of input string, unhexes the special chars, 
+	; Makes a COPY of input string, unhexes the special chars,
 	;	turns its '+'s into blanks (Windows search encodes space this as '+'),
 	;   removes a bunch of characters, trims blanks from head and tail,
-	;	and compresses mult whitespace 
+	;	and compresses mult whitespace
 	sanitize-query: func[str [string!]][
 		trim/lines trim/with replace/all dehex copy str #"+" #" " {"&%<>/"}
+	]
+	sanitize-result-string: func[str [any-type!]][
+		trim/lines replace/all replace/all copy str #"^M" #"," to-char 146 #"'" ; 146 is the weird right quote
+		; replace/all replace/all replace/all replace/all replace/all trim/lines copy str #"&" "&amp;" #"^M" #"," #"<" "&lt;" #"]" "xxxxxxxxxxxxxxxxxxx" #"'" "yyyyyyyyy"
 	]
 
 	get-request-from: use [request][
@@ -63,7 +70,7 @@ opensearch: use [
 			"Returns the search term from in-port"
 			in-port [port!] "port to read from"
 			/local buffer-in
-		][ 
+		][
 		    ; build up the request buffer
 			clear request: head request while [not empty? buffer-in: first in-port][
 				request: insert request reduce [buffer-in newline]
@@ -81,47 +88,51 @@ opensearch: use [
 		attempt [close server-port] server-port: open/lines to-url join "tcp://:" &conn/port
 
 		; if error? err: try [
-			forever [ 
-				if next-time < now/time [
-					log-app "Loading search data..." all [in &conn 'load &conn/load]
+			forever [
+				if now/time >= next-time [
+					log-app "Loading search data..."
+					all [in &conn 'load &conn/load]
 					next-time: now/time + 3599	 ; an hour from now
 				]
 				log-app ["Listening on..." server-port/port-id "next data refresh in" third next-time - now/time]
-		
+
 				; FIX the following back to something simple
-				results: any [
-					all [
-						minimum-length > length? searchTerm: sanitize-query get-request-from http-port: first wait server-port 
-						print "The search term is too short to send" []
-					]
-					&conn/search searchTerm [] 
+				results: any [all [minimum-length <= length? searchTerm: sanitize-query get-request-from http-port: first wait server-port &conn/search searchTerm]
+					all ["The search term is too short to send" []]
 				]
-			    log-app ["There are" length? results "rows with" searchTerm]
-			    clear buffer-out insert buffer-out rejoin [
-			    	{<?xml version="1.0" encoding="UTF-8"?>}
-					{<rss version="2.0" xmlns:media="http://search.yahoo.com/mrss" xmlns:opensearch="http://a9.com/-/spec/opensearchdescriptionch/1.1/" xmlns:atom="http://www.w3.org/2005/Atom">}
-						{<channel><title>} &conn/title 
-							{</title><link>} &conn/link 
-							{</link><description>Search results for} searchTerm 
-							{</description><opensearch:totalResults>} length? results 
-							{</opensearch:totalResults><opensearch:itemsPerPage>} length? results 
-							{</opensearch:itemsPerPage><opensearch:startIndex>} 1 
+			   	log-app ["There are" length? results "rows with" searchTerm]
+
+			   	clear buffer-out insert buffer-out join
+				    	{<?xml version="1.0" encoding="UTF-8"?>} [
+					{<rss version="2.0" xmlns:win="http://schemas.microsoft.com/windows/2008/propertynamespace" xmlns:media="http://search.yahoo.com/mrss" xmlns:opensearch="http://a9.com/-/spec/opensearchdescriptionch/1.1/" xmlns:atom="http://www.w3.org/2005/Atom">}
+						{<channel><title>} &conn/title
+							{</title><link>} &conn/link
+							{</link><description>Search results for } searchTerm
+							{</description><opensearch:totalResults>} length? results
+							{</opensearch:totalResults><opensearch:itemsPerPage>} length? results
+							{</opensearch:itemsPerPage><opensearch:startIndex>} 1
 							{</opensearch:startIndex><atom:link rel="search" type="application/opensearchdescription+xml" href="http://example.com/opensearchdescription.xml"/>}
-							{<opensearch:Query role="request" searchTerms="} searchTerm 
-							{" startPage="1" />} rejoin [
-					    		map-each value-map results [
-									rejoin [ 
-						    		; USE PARSE FOR THIS!!!!:
-										{<item>} 
-										if p*: select value-map 'title [join {<title><![CDATA[} [p* {]]></title>}]]
-										if p*: select value-map 'author [join {<author><![CDATA[} [p* {]]></author>}]]
-										if p*: select value-map 'link [join {<link><![CDATA[} [p* {]]></link>}]]
-										if p*: select value-map 'description [join {<description><![CDATA[} [p* {]]></description>}]]
-										if p*: select value-map 'thumbnail [join {<media:thumbnail url="} [p* {"/>}]]
-										{</item>}
-									]
+							{<opensearch:Query role="request" searchTerms="} searchTerm
+							{" startPage="1" />}
+					    		; USE PARSE FOR THIS!!!!: and SANITIZE AMPERSANDS HERE instead of in the
+					    	all [not empty? results map-each value-map results [
+					   ;  			unless parse results [some ['title set title] [
+								; 	? qb-connect
+								; 	if not value? 'arg-usr[to-error "missing email to authenticate"]
+								; 	if not value? 'arg-pass[to-error "missing password to authenticate"]
+								; 	to-error "unrecognized settings"
+								; ]
+					    		join {<item>} [
+									if p*: select value-map 'title [join {<title><![CDATA[} [sanitize-result-string p* {]]></title>}]]
+									if p*: select value-map 'author [join {<author><![CDATA[} [sanitize-result-string p* {]]></author>}]]
+									if p*: select value-map 'link [join {<link><![CDATA[} [sanitize-result-string p* {]]></link>}]]
+									if p*: select value-map 'description [join {<description><![CDATA[} [sanitize-result-string p* {]]></description>}]]
+									if p*: select value-map 'thumbnail [join {<media:thumbnail url="} [sanitize-result-string p* {"/>}]]
+									if p*: select value-map 'content [join {<media:content url="} [sanitize-result-string p* {"/>}]]
+									if p*: select value-map 'category [join {<media:category><![CDATA[} [sanitize-result-string p* {]]></media:category>}]]
+									{</item>}
 								]
-					    	]
+							]]
 						</channel> </rss>
 				]
 				unset [searchTerm results p*]
